d6deadacec4f6048aeb5454b35652720a0dc954a
Ross Allen <ross.a@protonmail.ch>
2021-12-04 22:56:52 +0200

    Second Commit, practicing from class - Using superclasses

---
+++ src/com/thatguyintheglass/Labrador.java
@@ -0,0 +1,10 @@
+package com.thatguyintheglass;
+
+public class Labrador extends Dog{
+    public Labrador(String color)
+    {
+
+        super(color,"Labrador");
+    }
+
+}
--- src/com/thatguyintheglass/Main.java
+++ src/com/thatguyintheglass/Main.java
@@ -3,7 +3,7 @@ package com.thatguyintheglass;
 public class Main {
 
     public static void main(String[] args) {
-        Dog myDog = new Dog("black","labrador");
+        Labrador myDog = new Labrador("black");
         System.out.println("I'm a big " + myDog.Color + " " + myDog.Type);
         myDog.MakeSound();
 
