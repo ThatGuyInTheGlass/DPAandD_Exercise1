cb0be103ef39ad3effd72ab4b2c6e012b93d1203
Ross Allen <ross.a@protonmail.ch>
2021-12-04 20:36:00 +0200

    First commit

--- src/com/thatguyintheglass/Main.java
+++ src/com/thatguyintheglass/Main.java
@@ -3,6 +3,6 @@ package com.thatguyintheglass;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is my first program in java");
+        System.out.println("This is my first program in java- Change");
     }
 }
